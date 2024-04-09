c9307527b1dd2eaf8208c1890a7f5880cccfad0a
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2024-04-09 20:36:14 -0300

    feat: cria função menu

---
+++ problema_grupo.py
@@ -0,0 +1,29 @@
+def menu():
+    print("--------------------------------")
+    print("Calculadora simples")
+    print("--------------------------------")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("0 - Sair")
+    print("--------------------------------")
+    
+    try:
+        option = int(input("Digite a opção desejada: "))
+        if option == 1:
+            sum()
+        elif option == 2:
+            subtraction()
+        elif option == 3:
+            multiplication()
+        elif option == 4:
+            division()
+        elif option == 0:
+            print("Obrigado por utilizar a calculadora")
+            print("\n")
+        else:
+            raise Exception
+    except:
+        print("Opção inválida")
+        print("\n")
